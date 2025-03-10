da0bc87efdc3f109ce210fca3ceb0c9f04919683
shmelrad <shmelrad@gmail.com>
2025-03-10 22:21:52 +0300

    add API prefix

--- nestodo_server/src/main.ts
+++ nestodo_server/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Nestodo API')
