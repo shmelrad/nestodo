9b7a05f1499caf4e2af69c3534ce0e2db5a0ece7
shmelrad <shmelrad@gmail.com>
2025-03-12 23:03:14 +0300

    match header and sidebar bg color

--- nestodo_client/src/pages/dashboard/DashboardHeader.tsx
+++ nestodo_client/src/pages/dashboard/DashboardHeader.tsx
@@ -23,7 +23,7 @@ export function DashboardHeader({ workspaceTitle, boardTitle }: DashboardHeaderP
     const { toggleSidebar } = useSidebar()
 
     return (
-        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="sticky top-0 z-50 w-full border-b bg-sidebar">
             <div className="container flex h-14 max-w-screen-2xl items-center">
                 <Button
                     variant="ghost"
