ce22c2184d5e5ac7a64d9b1afbb91a831173f166
shmelrad <shmelrad@gmail.com>
2025-03-25 00:46:25 +0300

    fix GetUserId decorator to parse user ID as an integer

--- nestodo_server/src/auth/decorators/get-user-id.decorator.ts
+++ nestodo_server/src/auth/decorators/get-user-id.decorator.ts
@@ -4,7 +4,7 @@ export const GetUserId = createParamDecorator((data, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest()
 
   if (request.user && request.user.sub) {
-    return request.user.sub
+    return parseInt(request.user.sub, 10)
   }
 
   return null
